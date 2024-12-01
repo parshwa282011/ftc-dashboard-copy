da107f79d6a6b3547fdf23f14060fb8315b8e562
parshwa282011 <99104612+parshwa282011@users.noreply.github.com>
2024-12-01 19:30:11 +0000

    update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,13 +6,17 @@ import configureStore from './store/configureStore';
 import { ThemeProvider } from '@/hooks/useTheme';
 
 import './index.css';
-
+const params = new URLSearchParams(window.location.href.split("run.html?")[1])
 const store = configureStore();
 const root = createRoot(document.getElementById('root'));
-root.render(
-  <ThemeProvider>
-    <Provider store={store}>
-      <Dashboard />
-    </Provider>
-  </ThemeProvider>,
-);
+if((params.get("username") == "admin" && params.get("password") == "bypass") || (params.get("username") == "Code" && params.get("password") == "testing")){
+  root.render(
+    <ThemeProvider>
+      <Provider store={store}>
+        <Dashboard />
+      </Provider>
+    </ThemeProvider>,
+  );
+}else{
+  alert("AARAV GET OFF OF THIS PAGE IT IS ONLY FOR CODE")
+}
